1d53c07a6693b71fa4dcc9debdeea7f79396deb9
Bahman <bahman.p@mtnirancell.ir>
2022-02-15 23:37:15 +0330

    using useSelector to be able to bring states inside the component

--- src/components/repositoriesList.tsx
+++ src/components/repositoriesList.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { useAction } from "./hooks/useActions";
 
 const RepositoriesList: React.FC = () => {
     const [term, setTerm] = useState("");
     const {searchRepositories} = useAction();
+    const {data, loading, error} = useSelector((state: any) => state.repositories);
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -16,6 +18,9 @@ const RepositoriesList: React.FC = () => {
                 <input value={term} type="text" onChange={(event) => setTerm(event.target.value)} />
                 <button>Search</button>
             </form>
+            {error && <h3>{error}</h3>}
+            {loading && <h3>Loading....</h3>}
+            {!error && !loading && data.map((name: string) => <div key={name}>{name}</div>)}
         </div>
     )
 }
